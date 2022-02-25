5de81f27e7e6b4bbacc377d82ce33376134fb454
aluthfi22 <73270416+aluthfi22@users.noreply.github.com>
2022-02-25 10:31:51 +0700

    Add files via upload

---
+++ Javascript refresher/4array.js
@@ -0,0 +1,6 @@
+const hobbies = ["sports", "cooking"];
+//for (let hobby of hobbies) {
+//  console.log(hobby);
+//}
+console.log(hobbies.map((hobby) => "hobi: " + hobby));
+console.log("hobi " + hobbies);
