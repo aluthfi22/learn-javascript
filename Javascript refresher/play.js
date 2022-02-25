bf7c38756dd966d8c3bb45d33385fb05653c3c83
aluthfi22 <73270416+aluthfi22@users.noreply.github.com>
2022-02-25 07:39:36 +0700

    Add files via upload

---
+++ Javascript refresher/play.js
@@ -0,0 +1,13 @@
+let name = "max";
+const umur = 24; //kalo gamau ubah ubah lagi
+var wayolo = false;
+
+name = "uyo";
+umur = 25;
+
+function cobain(userName, userAge, userWayolo) {
+  return (
+    "Namamu " + userName + ", Umur Jeneng " + userAge + ", Wayolo " + userWayolo
+  );
+}
+console.log(cobain(name, umur, wayolo));
